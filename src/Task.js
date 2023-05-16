ec2d1608e5115194c1618961e3eacb9a5619fd5c
kubrasertkaya <118736856+kubrasertkaya@users.noreply.github.com>
2023-05-16 23:38:47 +0300

    Tailwindscss ve date-fns

--- src/Task.js
+++ src/Task.js
@@ -1,11 +1,23 @@
 import React from "react";
-
+import { formatDistanceToNow ,differenceInDays} from 'date-fns';
+import {tr} from "date-fns/locale";
 const Task = ({ taskObj, onComplete }) => {
+  const deadline=new Date(taskObj.deadline);
+  const formatDeadline=formatDistanceToNow(deadline,{
+    addSuffix:true,
+    locale:tr
+  });
+  const isCloseToDeadline=differenceInDays(deadline,new Date())<=3;
+  
 
   return (
     <div className="task">
       <h3>{taskObj.title}</h3>
-      <div className="deadline">son teslim: <span>{taskObj.deadline}</span></div>
+      <div className="text-xs pt-1">
+        son teslim: {""}
+        <span className={isCloseToDeadline ? "bg-[#ffd9d4]":""}>{formatDeadline}</span>
+      </div>
+  
       <p>{taskObj.description}</p>
       <div>
         {taskObj.people.map((p) => (
